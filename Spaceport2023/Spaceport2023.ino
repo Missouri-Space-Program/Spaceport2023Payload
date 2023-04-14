ab8c8b1b508db2b96c8da5f8a99fc3d5f4518d97
Ryan Milewski <rsmbby@umsystem.edu>
2023-04-14 13:31:42 -0500

    RTC & SD Card working functions

--- Spaceport2023/Spaceport2023.ino
+++ Spaceport2023/Spaceport2023.ino
@@ -1,74 +1,23 @@
-
-//Libraries and definitions needed to get SD card on the Feather working
+#include <TimeLib.h>
 #include <SD.h>
 #include <SPI.h>
-#include "RTClib.h"
-#define cardSelect 4
 
-RTC_DS3231 rtc;
-//Pins in use by the acceleormeters
-const int x1Input = A0;
-const int y1Input = A1;
-const int z1Input = A2;
+const int chipSelect = BUILTIN_SDCARD;
 
-const int x2Input = A3;
-const int y2Input = A4;
-const int z2Input = A5;
-// Raw Ranges:
-
-//These ranges were determined after callibration, so default on each axis should be around 1G
-int x1RawMin = 490;
-int x1RawMax = 528;
-
-int y1RawMin = 491;
-int y1RawMax = 533;
-
-int z1RawMin = 494;
-int z1RawMax = 532;
-
-
-int x2RawMin = 492;
-int x2RawMax = 530;
-
-int y2RawMin = 491;
-int y2RawMax = 530;
-
-int z2RawMin = 494;
-int z2RawMax = 531;
-
-// Take multiple samples to reduce background noise
-const int sampleSize = 10;
-
-//Variables for our file that will be used to log data
-char filename[15];
 File logger;
+char filename[15];
+void setup()  {
+  // set the Time library to use Teensy 3.0's RTC to keep time
+  setSyncProvider(getTeensy3Time);
 
-
-//This function gets the date and time from the RTC to be used to modify the timestamp on the file itself
-void dateTime(uint16_t* date, uint16_t* time)
-{
-  DateTime now = rtc.now();
-  *date = FAT_DATE(now.year(), now.month(), now.day());
-  *time = FAT_TIME(now.hour(), now.minute(), now.second());
-}
-
-void setup() 
-{
-  //Open up serial communications on 5700 baud (This will be removed in final push)
-  Serial.begin(57600);
-  //Print out warning message if the RTC couldn't be found attached
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
+  Serial.begin(115200); 
+  if (timeStatus()!= timeSet) {
+    Serial.println("Unable to sync with the RTC");
+  } else {
+    Serial.println("RTC has set the system time");
   }
-
-  //Create a reference pin that our accelerometers will use
-  analogReference(AR_EXTERNAL);
-  //Set the date/time to be modified on the file itself
-  SdFile::dateTimeCallback(dateTime);
-
   //Check if the SD card can begin on the specified pin, if not then print an error message
-  if(!SD.begin(cardSelect)) {
+  if(!SD.begin(chipSelect)) {
     Serial.println("Card initalization failed!");
   }
   //Because strings in C are literals, we have to use strcpy to set our file name up for use in the file system.
@@ -95,79 +44,76 @@ void setup()
 }
 
 void loop() {
-  DateTime now = rtc.now();
-  //Use our getAxis function to get the raw data from the accelerometer
-  long x1Raw = getAxis(x1Input);
-  long y1Raw = getAxis(y1Input);
-  long z1Raw = getAxis(z1Input);
-
-  long x2Raw = getAxis(x2Input);
-  long y2Raw = getAxis(y2Input);
-  long z2Raw = getAxis(z2Input);
-
-  // Convert raw values to 'milli-Gs" by using the map function
-  long x1Scaled = map(x1Raw, x1RawMin, x1RawMax, -1000, 1000);
-  long y1Scaled = map(y1Raw, y1RawMin, y1RawMax, -1000, 1000);
-  long z1Scaled = map(z1Raw, z1RawMin, z1RawMax, -1000, 1000);
-
-  long x2Scaled = map(x2Raw, x2RawMin, x2RawMax, -1000, 1000);
-  long y2Scaled = map(y2Raw, y2RawMin, y2RawMax, -1000, 1000);
-  long z2Scaled = map(z2Raw, z2RawMin, z2RawMax, -1000, 1000);
-
-  // re-scale to fractional Gs
-  float x1Accel = x1Scaled / 1000.0;
-  float y1Accel = y1Scaled / 1000.0;
-  float z1Accel = z1Scaled / 1000.0;
-
-  float x2Accel = x2Scaled / 1000.0;
-  float y2Accel = y2Scaled / 1000.0;
-  float z2Accel = z2Scaled / 1000.0;
-    
-  //Reopen the file to be written to
-  logger = SD.open(filename, FILE_WRITE);
-  //Begin to print our data to the file
-
-  logger.print(now.month(), DEC);
-  logger.print('/');
-  logger.print(now.day(), DEC);
-  logger.print('/');
-  logger.print(now.year(), DEC);
-  logger.print(' ');
-  logger.print(now.hour(), DEC);
-  logger.print(':');
-  logger.print(now.minute(), DEC);
-  logger.print(':');
-  logger.print(now.second(), DEC);
-
-  logger.print(" ");
-  logger.print(rtc.getTemperature());
-  logger.print(' ');
-  logger.print(x1Accel);
-  logger.print(' ');
-  logger.print(y1Accel);
-  logger.print(' ');
-  logger.print(z1Accel);
-
-  logger.print(' ');
-  logger.print(x2Accel);
-  logger.print(' ');
-  logger.print(y2Accel);
-  logger.print(' ');
-  logger.print(z2Accel);
-  logger.println();
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //Finally close the file when done writing to get the data to write to the SD card
+  time_t t = processSyncMessage();
+  if (t != 0) {
+    Teensy3Clock.set(t); // set the RTC
+    setTime(t);
+  }
+  digitalClockDisplay();
+  logger = SD.open(filename, FILE_WRITE);  
+  SDClockDisplay();
   logger.close();
+  //delay(1000);
+}
+void SDClockDisplay() {
+  // digital clock display of the time
+  logger.print(hour());
+  printSDDigits(minute());
+  printSDDigits(second());
+  logger.print(" ");
+  logger.print(day());
+  logger.print(" ");
+  logger.print(month());
+  logger.print(" ");
+  logger.print(year()); 
+  logger.println(); 
+}
+void digitalClockDisplay() {
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
 }
 
-//This function will take in a given pin and return the raw reading of the axis given as an integer
-int getAxis(int pin) {
-  //Create a result to store each sample added up
-  long result = 0;
-  //Go through and read off the pin the set number of times (10) and add them up
-  for(int i = 0; i < sampleSize; i++) {
-    result += analogRead(pin);
+time_t getTeensy3Time()
+{
+  return Teensy3Clock.get();
+}
+
+/*  code to process time sync messages from the serial port   */
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+
+unsigned long processSyncMessage() {
+  unsigned long pctime = 0L;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013 
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     return pctime;
+     if( pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
+       pctime = 0L; // return 0 to indicate that the time is not valid
+     }
   }
-  //Finally return the average (divided by the sample size)
-  return result / sampleSize;
+  return pctime;
+}
+void printSDDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  logger.print(":");
+  if(digits < 10)
+    logger.print('0');
+  logger.print(digits);
+}
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
 }
