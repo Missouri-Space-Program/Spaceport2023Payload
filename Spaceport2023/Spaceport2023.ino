1379e9ba85310a7b28dbb9945c1ba11598d7200a
Ryan Milewski <rsmbby@umsystem.edu>
2023-05-12 14:26:08 -0500

    Update to include functionality for all 3 accel.

--- Spaceport2023/Spaceport2023.ino
+++ Spaceport2023/Spaceport2023.ino
@@ -4,11 +4,19 @@
 
 const int chipSelect = BUILTIN_SDCARD;
 
-const int A1xInput = A2;
-const int A1yInput = A3;
-const int A1zInput = A4;
+const int A1xInput = A16;
+const int A1yInput = A1;
+const int A1zInput = A0;
 
-int A1xRawMin = 381;
+const int A2xInput = A2;
+const int A2yInput = A3;
+const int A2zInput = A4;
+
+const int A3xInput = A12;
+const int A3yInput = A11;
+const int A3zInput = A10;
+
+int A1xRawMin = 380;
 int A1xRawMax = 410;
 
 int A1yRawMin = 382;
@@ -16,6 +24,24 @@ int A1yRawMax = 412;
 
 int A1zRawMin = 385;
 int A1zRawMax = 414;
+
+int A2xRawMin = 380;
+int A2xRawMax = 410;
+
+int A2yRawMin = 381;
+int A2yRawMax = 411;
+
+int A2zRawMin = 382;
+int A2zRawMax = 412;
+
+int A3xRawMin = 381;
+int A3xRawMax = 410;
+
+int A3yRawMin = 586;
+int A3yRawMax = 616;
+
+int A3zRawMin = 589;
+int A3zRawMax = 620;
 
 const int sampleSize = 10;
 
@@ -56,15 +82,15 @@ void setup()  {
   logger.close();
   //Allows for LED light on Teensy to be turned on
   pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void loop() {
   time_t t = processSyncMessage();
   if (t != 0) {
     Teensy3Clock.set(t); // set the RTC
     setTime(t);
   }
-  long A1xRaw = analogRead(A1xInput);
+}
+
+void loop() {
+  /*long A1xRaw = analogRead(A1xInput);
   long A1yRaw = analogRead(A1yInput);
   long A1zRaw = analogRead(A1zInput);
 
@@ -80,23 +106,65 @@ void loop() {
   Serial.print(A1yAccel);
   Serial.print("G Z1:");
   Serial.print(A1zAccel);
-  Serial.print("G");
-  digitalClockDisplay();
+  Serial.print("G ");
+  long A2xRaw = analogRead(A2xInput);
+  long A2yRaw = analogRead(A2yInput);
+  long A2zRaw = analogRead(A2zInput);
+
+  long A2xScaled = map(A2xRaw,A2xRawMin,A2xRawMax, -1000,1000);
+  long A2yScaled = map(A2yRaw,A2yRawMin,A2yRawMax, -1000,1000);
+  long A2zScaled = map(A2zRaw,A2zRawMin,A2zRawMax, -1000,1000);
+  float A2xAccel = A2xScaled / 1000.0;
+  float A2yAccel = A2yScaled / 1000.0;
+  float A2zAccel = A2zScaled / 1000.0;
+  Serial.print(" X2:");
+  Serial.print(A2xAccel);
+  Serial.print("G Y2:");
+  Serial.print(A2yAccel);
+  Serial.print("G Z2:");
+  Serial.print(A2zAccel);
+  Serial.print("G ");
+
+  long A3xRaw = analogRead(A3xInput);
+  long A3yRaw = analogRead(A3yInput);
+  long A3zRaw = analogRead(A3zInput);
+
+  long A3xScaled = map(A3xRaw,A3xRawMin,A3xRawMax, -1000,1000);
+  long A3yScaled = map(A3yRaw,A3yRawMin,A3yRawMax, -1000,1000);
+  long A3zScaled = map(A3zRaw,A3zRawMin,A3zRawMax, -1000,1000);
+  float A3xAccel = A3xScaled / 1000.0;
+  float A3yAccel = A3yScaled / 1000.0;
+  float A3zAccel = A3zScaled / 1000.0;
+  Serial.print(" X3:");
+  Serial.print(A3xAccel);
+  Serial.print("G Y3:");
+  Serial.print(A3yAccel);
+  Serial.print("G Z3:");
+  Serial.print(A3zAccel);
+  Serial.print("G ");
+  */
+  //printAccOutput(A1xInput,A1yInput,A1zInput, A1xRawMin,A1xRawMax, A1yRawMin, A1yRawMax, A1zRawMin, A1zRawMax);
+  //printAccOutput(A2xInput,A2yInput,A2zInput, A2xRawMin,A2xRawMax, A2yRawMin, A2yRawMax, A2zRawMin, A2zRawMax);
+  //printAccOutput(A3xInput,A3yInput,A3zInput, A3xRawMin,A3xRawMax, A3yRawMin, A3yRawMax, A3zRawMin, A3zRawMax);
+  //digitalClockDisplay();
   logger = SD.open(filename, FILE_WRITE);  
+  printSDAccOutput(logger,A1xInput,A1yInput,A1zInput, A1xRawMin,A1xRawMax, A1yRawMin, A1yRawMax, A1zRawMin, A1zRawMax);
+  printSDAccOutput(logger,A2xInput,A2yInput,A2zInput, A2xRawMin,A2xRawMax, A2yRawMin, A2yRawMax, A2zRawMin, A2zRawMax);
+  printSDAccOutput(logger,A3xInput,A3yInput,A3zInput, A3xRawMin,A3xRawMax, A3yRawMin, A3yRawMax, A3zRawMin, A3zRawMax);
   SDClockDisplay();
   logger.close();
-  //delay(1000);
+  digitalWrite(LED_BUILTIN,HIGH);
 }
 void SDClockDisplay() {
   // digital clock display of the time
   logger.print(hour());
   printSDDigits(minute());
   printSDDigits(second());
-  logger.print(" ");
+  logger.print(",");
   logger.print(day());
-  logger.print(" ");
+  logger.print(",");
   logger.print(month());
-  logger.print(" ");
+  logger.print(",");
   logger.print(year()); 
   logger.println(); 
 }
@@ -149,14 +217,39 @@ void printDigits(int digits){
     Serial.print('0');
   Serial.print(digits);
 }
-//This function will take in a given pin and return the raw reading of the axis given as an integer
-int getAxis(int pin) {
-  //Create a result to store each sample added up
-  long result = 0;
-  //Go through and read off the pin the set number of times (10) and add them up
-  for(int i = 0; i < sampleSize; i++) {
-    result += analogRead(pin);
-  }
-  //Finally return the average (divided by the sample size)
-  return result / sampleSize;
+void printAccOutput(int xInput, int yInput, int zInput, int xRawMin, int xRawMax, int yRawMin, int yRawMax, int zRawMin, int zRawMax) {
+  long xRaw = analogRead(xInput);
+  long yRaw = analogRead(yInput);
+  long zRaw = analogRead(zInput);
+
+  long xScaled = map(xRaw,xRawMin,xRawMax, -1000,1000);
+  long yScaled = map(yRaw,yRawMin,yRawMax, -1000,1000);
+  long zScaled = map(zRaw,zRawMin,zRawMax, -1000,1000);
+  float xAccel = xScaled / 1000.0;
+  float yAccel = yScaled / 1000.0;
+  float zAccel = zScaled / 1000.0;
+  Serial.print(xAccel);
+  Serial.print("G ");
+  Serial.print(yAccel);
+  Serial.print("G ");
+  Serial.print(zAccel);
+  Serial.print("G ");
+}
+void printSDAccOutput(File logger, int xInput, int yInput, int zInput, int xRawMin, int xRawMax, int yRawMin, int yRawMax, int zRawMin, int zRawMax) {
+  long xRaw = analogRead(xInput);
+  long yRaw = analogRead(yInput);
+  long zRaw = analogRead(zInput);
+
+  long xScaled = map(xRaw,xRawMin,xRawMax, -1000,1000);
+  long yScaled = map(yRaw,yRawMin,yRawMax, -1000,1000);
+  long zScaled = map(zRaw,zRawMin,zRawMax, -1000,1000);
+  float xAccel = xScaled / 1000.0;
+  float yAccel = yScaled / 1000.0;
+  float zAccel = zScaled / 1000.0;
+  logger.print(xAccel);
+  logger.print(",");
+  logger.print(yAccel);
+  logger.print(",");
+  logger.print(zAccel);
+  logger.print(",");
 }
